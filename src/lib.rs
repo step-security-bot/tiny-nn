7d8d7c054b6ff4b8468d0322522eee5464b44f44
rommeld <rommel.denny@gmail.com>
2024-03-14 19:17:27 +0100

    Initialize random parameters.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rand::prelude::*;
+// SGD - Stochastic Gradient Descent
+// Initialize parameters (model weights) to set path for algorithm
+// TODO: Randomly select data points from dataset
+// TODO: Calculate gradient for loss function of the selected data points
+pub fn stoch_gradient_descent(dataset: Vec<f32>) -> Vec<f32> {
+    let mut rng = thread_rng();
+    let weights: Vec<f32> = vec![rng.gen(); dataset.len()];
+    weights
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +16,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let dataset = vec![0.1, -0.2, 0.2, 0.3, 0.9];
+        let result = stoch_gradient_descent(dataset);
+        assert_eq!(result.len(), 5);
     }
 }
